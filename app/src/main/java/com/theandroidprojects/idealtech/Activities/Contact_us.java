ce32d0414b3cd6cf126e7be3ea0dd0ac174219aa
Adarsh1999 <adarshguptamaurya@gmail.com>
2018-06-24 00:40:33 +0530

    contact us and about us completed

--- app/src/main/java/com/theandroidprojects/idealtech/Activities/Contact_us.java
+++ app/src/main/java/com/theandroidprojects/idealtech/Activities/Contact_us.java
@@ -6,7 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
+
 import com.theandroidprojects.idealtech.R;
 
 public class Contact_us extends AppCompatActivity {
@@ -14,11 +15,10 @@ public class Contact_us extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contact_us_activity_scrolling);
-        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.contact_us_activiity_scrolling);
+        Toolbar toolbar1= (Toolbar)findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar1);
 
-        
 
 
     }
